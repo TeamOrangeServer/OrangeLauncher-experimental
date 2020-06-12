0c1e9d41dcfe4426bb0775e51f765873b4386e8a
sousuke0422 <sousuke20xx@gmail.com>
2020-06-12 14:34:26 +0900

    feat(ui): card

--- src/renderer/pages/settings.vue
+++ src/renderer/pages/settings.vue
@@ -1,63 +1,89 @@
 <template>
   <v-app>
     <div class="sp_s_top">
-      <p class="setting_title indigo white--text">Main Config</p>
+      <p class="setting_title indigo white--text">
+        Main Config
+      </p>
     </div>
     <v-card outlined left>
       <v-list-item three-line>
         <v-list-item-content>
-          <v-list-item-title class="headline mb-1"
-            >ダークモード切り替え</v-list-item-title
-          >
-          <v-divider></v-divider>
-          <v-list-item-subtitle
-            >OrangeLauncherのテーマを変更します。</v-list-item-subtitle
-          >
+          <v-list-item-title class="headline mb-1">
+            ダークモード切り替え
+          </v-list-item-title>
+          <v-divider />
+          <v-list-item-subtitle>
+            OrangeLauncherのテーマを変更します。
+          </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-card-actions>
         <dsw />
       </v-card-actions>
     </v-card>
-    <br />
+    <p class="card_bottom" />
     <v-card outlined left>
       <v-list-item three-line>
         <v-list-item-content>
-          <v-list-item-title class="headline mb-1"
-            >ハードウェアアクセラレーション無効化(未実装)</v-list-item-title
-          >
-          <v-divider></v-divider>
-          <v-list-item-subtitle
-            >※再起動が必要です。(未実装)</v-list-item-subtitle
-          >
+          <v-list-item-title class="headline mb-1">
+            ハードウェアアクセラレーション無効化(未実装)
+          </v-list-item-title>
+          <v-divider />
+          <v-list-item-subtitle>
+            ※再起動が必要です。(未実装)
+          </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-card-actions>
-          <v-switch></v-switch>
+        <v-switch />
       </v-card-actions>
     </v-card>
-    <hr class="sp_s_bottom" />
-    <p class="sp_s_bottom">その他</p>
-    <div class="sp_s_bottom text-center">
-      <v-btn to="edition" style="width: 90%;">他の配布形態</v-btn>
-    </div>
-    <div class="sp_s_bottom text-center">
-      <v-btn to="oss" style="width: 90%;">オープンソースライセンス</v-btn>
-    </div>
-    <hr class="sp_s_bottom" />
-    <div class="sp_s_bottom">
-      <p>バージョン</p>
-      <p id="lversion">{{ lversion || '不明' }}</p>
-    </div>
+    <p class="card_bottom" />
+    <v-card outlined left>
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-1">
+            その他
+          </v-list-item-title>
+          <v-divider />
+        </v-list-item-content>
+      </v-list-item>
+      <v-card-actions>
+        <v-container>
+          <div class="sp_s_bottom text-center">
+            <v-btn to="edition" style="width: 100%;">
+              他の配布形態
+            </v-btn>
+          </div>
+          <div class="sp_s_bottom text-center">
+            <v-btn to="oss" style="width: 100%;">
+              オープンソースライセンス
+            </v-btn>
+          </div>
+        </v-container>
+      </v-card-actions>
+    </v-card>
+    <p class="card_bottom" />
+    <v-card outlined left>
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-1">
+            バージョン
+          </v-list-item-title>
+          <v-divider />
+        </v-list-item-content>
+      </v-list-item>
+      <v-card-actions>
+        <v-container>
+          <p>{{ require('./../../../package.json').name }}: {{ require('./../../../package.json').version }}</p>
+        </v-container>
+      </v-card-actions>
+    </v-card>
   </v-app>
 </template>
 
 <script>
-
 import dsw from '~/components/DarkSwitch.vue'
-
-// ????
-const lversion = require('../../../package.json').version
 
 export default {
   components: {
